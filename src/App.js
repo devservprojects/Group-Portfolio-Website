3cec675fe408cbfa7e80079db0f567d5c5a1baaf
Won Tae Jung <wontae.andyjung@gmail.com>
2021-02-02 20:45:28 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="test">
+        <h1 style={{textAlign: "center"}}>
+        A<br/>
+        S<br/>
+        W
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
